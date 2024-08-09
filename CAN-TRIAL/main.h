63b82fe1c2aeeb853ff5ff78263a2c9ed437129a
riru <riru4293@gmail.com>
2024-08-09 18:34:59 +0900

    To minimal

--- CAN-TRIAL/main.h
+++ CAN-TRIAL/main.h
@@ -1,53 +1,27 @@
 /**
- * RP2040 FreeRTOS Template
- *
- * @copyright 2023, Tony Smith (@smittytone)
- * @version   1.4.2
- * @license   MIT
- *
+ * CAN communication trial
+ * 
+ * @copyright 2024, riru
+ * @version   0.0.2
+ * @licence   BSD-2-Clause
  */
 #ifndef MAIN_H
 #define MAIN_H
 
 
 // FreeRTOS
-#include <FreeRTOS.h>
-#include <task.h>
-#include <queue.h>
+// #include <FreeRTOS.h>
+// #include <task.h>
+// #include <queue.h>
 // C
-#include <stdbool.h>
-#include <stdlib.h>
+// #include <stdbool.h>
+// #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
+// #include <string.h>
+// #include <time.h>
 // Pico SDK
 #include "pico/stdlib.h"            // Includes `hardware_gpio.h`
-#include "pico/binary_info.h"
-
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-/**
- * CONSTANTS
- */
-#define         RED_LED_PIN           20
-
-
-/**
- * PROTOTYPES
- */
-void led_task_pico(void* unused_arg);
-void led_task_gpio(void* unused_arg);
-void log_debug(const char* msg);
-void log_device_info(void);
-
-
-#ifdef __cplusplus
-}           // extern "C"
-#endif
+// #include "pico/binary_info.h"
 
 
 #endif      // MAIN_H
