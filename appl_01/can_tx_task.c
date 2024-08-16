98b52d22c8c2e7d78e65f746ba3f313292f120a9
riru <riru4293@gmail.com>
2024-08-16 18:06:45 +0900

    試作タイマーを追加

--- appl_01/can_tx_task.c
+++ appl_01/can_tx_task.c
@@ -3,6 +3,7 @@
 // FreeRTOS
 #include <FreeRTOS.h>
 #include <task.h>
+#include <timers.h>
 
 #include "driver/include/public/driver.h"
 #include "logger/include/public/logger.h"
@@ -19,7 +20,12 @@ static TaskHandle_t task_handle = NULL;
 
 
 /* temp */
+#ifdef DEBUG
 #define SPICMD_REQ_TX0                  ( 0x81U )
+TimerHandle_t xAutoReloadTimer;
+VOID autoReloadTimerCallback( TimerHandle_t xTimer );
+BaseType_t xTimer2Started; 
+#endif
 
 
 VOID create_can_tx_task( VOID )
@@ -34,8 +40,20 @@ TaskHandle_t get_can_tx_task_handler( VOID )
 }
 
 
+#ifdef DEBUG
+    VOID autoReloadTimerCallback( TimerHandle_t xTimer )
+    {
+        printf("tick: %d\n", time_us_32());
+    }
+#endif
+
+
 static VOID task( VOID* unused_arg )
 {
+#ifdef DEBUG
+    xAutoReloadTimer = xTimerCreate("Reload" , 1000, pdTRUE , NULL, autoReloadTimerCallback);
+    xTimer2Started = xTimerStart( xAutoReloadTimer, 0 ); 
+#endif
     while( TRUE )
     {
         ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
