aaa6298fa3c062dfef6635d74b08f5ba2476a334
riru <riru4293@gmail.com>
2024-09-14 19:11:19 +0900

    引数のconst化

--- hardware_driver/include/hardware_driver.h
+++ hardware_driver/include/hardware_driver.h
@@ -13,9 +13,9 @@ typedef VOID (*HWDRV_CAN_IRQ_CALLBACK_T)( VOID );
 /* Prototype                                                                  */
 /* -------------------------------------------------------------------------- */
 VOID hwdrv_init_hardware( VOID );
-VOID hwdrv_light_led_1( BOOL lit );
+VOID hwdrv_light_led_1( const BOOL lit );
 ERR_CD_T hwdrv_reset_can_controller( VOID );
-VOID hwdrv_set_can_irq_callback( HWDRV_CAN_IRQ_CALLBACK_T callback );
-VOID hwdrv_enable_can_irq( BOOL enabled );
+VOID hwdrv_set_can_irq_callback( const HWDRV_CAN_IRQ_CALLBACK_T const callback );
+VOID hwdrv_enable_can_irq( const BOOL enabled );
 
 #endif /* HARDWARE_DRIVER_H */
